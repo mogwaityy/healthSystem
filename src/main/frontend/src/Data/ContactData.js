28742ac355cc84944cce25fe4177073c757f4f6d
mogwaityy <147443420+mogwaityy@users.noreply.github.com>
2025-06-19 10:43:30 +0800

    chore: translate user prompts to Chinese

--- src/main/frontend/src/Data/ContactData.js
+++ src/main/frontend/src/Data/ContactData.js
@@ -1,25 +1,25 @@
 export const ContactData = [
     {
         icon: <i className='fa fa-phone'></i>,
-        method: "EMERGENCY",
+        method: "紧急电话",
         content1:"+44 12345678",
         content2:"+44 87654321"
     },
     {
         icon: <i className='fa fa-envelope'></i>,
-        method: "EMAIL",
+        method: "电子邮件",
         content1:"contact@eclinic.com",
         content2:"support@eclinic.com"
     },
     {
         icon: <i className='fa fa-location-dot'></i>,
-        method: "LOCATION",
+        method: "地址",
         content1:"SO00 3ET, Southampton",
         content2:"SO11 3ET, Southampton"
     },   
     {
         icon: <i className='fa fa-clock'></i>,
-        method: "WORKING HOUR",
+        method: "工作时间",
         content1:"Mon-Sat 09:00-20:00",
         content2:"Sunday 09:00-17:00"
     }
